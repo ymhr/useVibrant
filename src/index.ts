a965277b1e99e790aad514003bedf491a7c0bcc3
James Martin <jamesmartin442@gmail.com>
2019-06-22 12:01:21 +0100

    Support more versions

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,37 @@
-import React from 'react';
-import Vibrant from 'node-vibrant';
-import { Palette } from 'node-vibrant/lib/color';
-import { ImageSource } from 'node-vibrant/lib/typing';
+import React from "react";
+import Vibrant from "node-vibrant";
+import { Palette } from "node-vibrant/lib/color";
+import { ImageSource } from "node-vibrant/lib/typing";
 
 const cache: { [url: string]: Palette } = {};
 
 export default function useVibrant(
-	url: ImageSource | null
+  url: ImageSource | null
 ): { colors: Palette; done: boolean } {
-	const [colors, setColors] = React.useState<Palette>({});
-	const [done, setDone] = React.useState<boolean>(false);
+  const [colors, setColors] = React.useState<Palette>({});
+  const [done, setDone] = React.useState<boolean>(false);
 
-	React.useEffect((): void => {
-		if (!url) {
-			return;
-		}
+  React.useEffect((): void => {
+    if (!url) {
+      return;
+    }
 
-		const urlString = url.toString();
+    const urlString = url.toString();
 
-		if (cache[urlString]) {
-			setColors(cache[urlString]);
-			setDone(true);
-		}
+    if (cache[urlString]) {
+      setColors(cache[urlString]);
+      setDone(true);
+    }
 
-		Vibrant.from(url)
-			.getPalette()
-			.then(
-				(palette: Palette): void => {
-					setColors(palette);
-					setDone(true);
+    Vibrant.from(url)
+      .getPalette()
+      .then((palette: Palette): void => {
+        setColors(palette);
+        setDone(true);
 
-					cache[urlString] = palette;
-				}
-			);
-	}, [url]);
+        cache[urlString] = palette;
+      });
+  }, [url]);
 
-	return { colors, done };
+  return { colors, done };
 }
